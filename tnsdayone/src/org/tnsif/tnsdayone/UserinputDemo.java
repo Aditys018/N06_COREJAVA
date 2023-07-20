cf3c1fbe2bc270eabe36432cd719dad0a1300d15
Aditys018 <aditis.comp_ioe@bkc.met.edu>
2023-07-20 14:07:17 +0530

    First Commit

---
+++ tnsdayone/src/org/tnsif/tnsdayone/UserinputDemo.java
@@ -0,0 +1,22 @@
+//Program to demonstrate on userInput
+//Scanner class
+//for different kind of variables we can use scanner class like this=
+//int num=s.nextInt();
+//float percentage=s.nextfloat();
+//char c=s.next().charAt(0);
+package org.tnsif.tnsdayone;
+
+import java.util.Scanner;
+
+public class UserinputDemo {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+	    System.out.println("Enter a string:");
+		String str1=s.nextLine();
+		System.out.println("String is: "+str1);
+		s.close()
+;		
+	}
+
+}
