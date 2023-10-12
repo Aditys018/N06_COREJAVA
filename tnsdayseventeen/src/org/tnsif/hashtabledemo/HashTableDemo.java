64290838156bb2015b901c16e3dd36a79c4730bf
Aditys018 <aditis.comp_ioe@bkc.met.edu>
2023-10-12 11:02:14 +0530

    Errors Resolved

--- tnsdayseventeen/src/org/tnsif/hashtabledemo/HashTableDemo.java
+++ tnsdayseventeen/src/org/tnsif/hashtabledemo/HashTableDemo.java
@@ -3,54 +3,61 @@ package org.tnsif.hashtabledemo;
 
 import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map.Entry;
 import java.util.Set;
 
 public class HashTableDemo {
 
 	public static void main(String[] args) {
+		// unordered, unsorted map
 		Hashtable<String, String> persons = new Hashtable<String, String>();
-		//if we insert the same values here then the current value will be replaced here
-		//if we insert a null value here then we will get the nullpointerException
-		
-		persons.put("adt@gmail.com" , "ADT");
-		persons.put("aadi@gmail.com" , "Aadi");
-		persons.put("aar@gmail.com" , "Aarti");
-		persons.put("aan@gmail.com" , "Aanchal");
-		persons.put("arp@gmail.com" , "Arpit");
-		
+
+		persons.put("aniket@gmail.com", "Aniket");
+		persons.put("nidhi@gmail.com", "Nidhi");
+		persons.put("lisha@gmail.com", "Lisha");
+		persons.put("dhruvi@gmail.com", "Dhruvi");
+		System.out.println(persons);
+
+		persons.put("lisha@gmail.com", "Lisha Sharma");
+		System.out.println(persons);
+		// null keys and values not allowed
+		// persons.put("abc@gmail.com",null);
+		// persons.put(null,"abc");
+		// persons.put(null,null);
 		System.out.println(persons);
 		
-		persons.put("arp@gmal.com" , "Arpit Sinha");
-		System.out.println(persons);
+		System.out.println(persons.containsKey("lisha@gmail.com"));
 		
-//		persons.put( null, "abc"); //exception
-//		System.out.println(persons);
-//		
-		System.out.println(persons.containsKey("arp@gmail.com"));
-		System.out.println(persons.containsKey("Arpit"));
-		
+		System.out.println(persons.containsValue("Lisha"));
+		System.out.println("-------------------------------");
+		//Retrieve only values
 		Enumeration<String> em= persons.elements();
 		while(em.hasMoreElements())
 			System.out.println(em.nextElement());
+
+		System.out.println("-------------------------------");
+		System.out.println(persons.get("lisha@gmail.com"));
 		
-		System.out.println("----------------------------------------------");
-		System.out.println(persons.get("aar@gmail.com"));
-		
-		System.out.println("--------------------------------------------------");
-		
-		Enumeration<String> keys = persons.keys();
+		System.out.println("-------------------------------");
+		Enumeration<String> keys= persons.keys();
 		while(keys.hasMoreElements())
 		{
-			String k = keys.nextElement();
-			System.out.println(k+ ":" + persons.get(k));
+			String k=keys.nextElement();
+			System.out.println(k+" : "+persons.get(k));
 		}
 		
-		System.out.println("------------------------------------------------------");
-		Set dictionaries= persons.entrySet();
 		
-		
-		
+		System.out.println("-------------------------------");
+		Set<Entry<String, String>> dict=persons.entrySet();
+		System.out.println(dict);
+		Iterator<Entry<String, String>> it=dict.iterator();
+		while(it.hasNext())
+		{
+			Entry<String, String> rec=it.next();
+			System.out.println(rec.getKey()+" : "+rec.getValue());
+		}
+		System.out.println("-------------------------------");
 		
 	}
-
 }
